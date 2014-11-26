edfddf55a65ba2aa4bb05b540a01a3e34e437c47
j4velin <j4velin@gmail.com>
2014-11-26 21:37:21 +0100

    fixed nullpointer exception, if location wasn't available yet

--- src/main/java/de/j4velin/mapsmeasure/Map.java
+++ src/main/java/de/j4velin/mapsmeasure/Map.java
@@ -424,16 +424,19 @@ public class Map extends FragmentActivity {
         mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
             @Override
             public boolean onMyLocationButtonClick() {
-                LatLng myLocation = new LatLng(mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude());
-                double distance = SphericalUtil.computeDistanceBetween(myLocation,
-                        mMap.getCameraPosition().target);
+                if (mMap.getMyLocation() != null) {
+                    LatLng myLocation = new LatLng(mMap.getMyLocation().getLatitude(),
+                            mMap.getMyLocation().getLongitude());
+                    double distance = SphericalUtil
+                            .computeDistanceBetween(myLocation, mMap.getCameraPosition().target);
 
-                // Only if the distance is less than 50cm we are on our location, add the marker
-                if (distance < 0.5) {
-                    Toast.makeText(Map.this, R.string.marker_on_current_location, Toast.LENGTH_SHORT).show();
-                    addPoint(myLocation);
+                    // Only if the distance is less than 50cm we are on our location, add the marker
+                    if (distance < 0.5) {
+                        Toast.makeText(Map.this, R.string.marker_on_current_location,
+                                Toast.LENGTH_SHORT).show();
+                        addPoint(myLocation);
+                    }
                 }
-
                 return false;
             }
         });
