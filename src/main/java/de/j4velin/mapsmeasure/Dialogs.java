9666c1a6c016a189f62809553332b425d1368a0a
j4velin <j4velin@gmail.com>
2018-05-12 22:49:19 +0200

    fix build

--- src/main/java/de/j4velin/mapsmeasure/Dialogs.java
+++ src/main/java/de/j4velin/mapsmeasure/Dialogs.java
@@ -29,7 +29,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
-import android.support.v4.BuildConfig;
 import android.support.v4.content.FileProvider;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
